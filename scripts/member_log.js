3f3b64ff2e4ce891c427e1a590f65280958098e7
chenluily0451 <chenluily@163.com>
2015-12-23 18:17:20 +0800

    update

--- scripts/member_log.js
+++ scripts/member_log.js
@@ -7,7 +7,21 @@ var memberLog={
 			else{
 				$(this).children("span").removeClass("uc_checkmarker_false").addClass("uc_checkmarker_true");
 			}
-		})
+		});
+	},
+	moblieCheck : function(){
+		 var mobileReg = /^1[3|4|5|8][0-9]\d{8}$/;
+		 $("#mobileNum").blur(function(){
+		 	if (mobileReg.test($("#mobileNum").val())){
+		 		$(this).siblings("i").addClass("uc_successIcon");
+		 		$(this).siblings(".errorInfo").text("");
+		 	}else{
+		 		$(this).siblings(".errorInfo").text("请输入正确的手机号");
+		 		$(this).siblings("i").addClass("uc_errorIcon");
+		 		return false;
+		 	}
+		 });
 	}
-}
+};
 memberLog.rememberName();
+memberLog.moblieCheck();
