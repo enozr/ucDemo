229feda1c16a25fc2637ff8a49410b48d9510b5d
chenluily0451 <chenluily@163.com>
2015-12-23 16:51:12 +0800

    这里写注释

--- scripts/member_log.js
+++ scripts/member_log.js
@@ -1,3 +1,13 @@
-document.ready(function(){
-	alert('asd')
-})
+var memberLog={
+	rememberName : function(){
+		$('#rememberBtn').on('click',function(){
+			if($(this).children("span").hasClass("uc_checkmarker_true")){
+				$(this).children("span").removeClass("uc_checkmarker_true").addClass("uc_checkmarker_false");
+			}
+			else{
+				$(this).children("span").removeClass("uc_checkmarker_false").addClass("uc_checkmarker_true");
+			}
+		})
+	}
+}
+memberLog.rememberName();
